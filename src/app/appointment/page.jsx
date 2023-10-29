455f8d2f904ce0ba86ed4ea27d9fdadc3cca4652
Chakrapani U <source.lastbenchcoder@gmail.com>
2023-10-29 08:04:59 +0530

    text color changed

--- src/app/appointment/page.jsx
+++ src/app/appointment/page.jsx
@@ -6,7 +6,7 @@ const Appointment = () => {
       <div className="container mx-auto flex flex-col md:flex-row items-center">
         {/* Left side (Google Map) */}
         <div className="md:w-1/2 mb-6 md:mb-0">
-        <img
+          <img
             src="banner-2.png"
             alt="About Us"
             className="w-full h-auto rounded-lg shadow-lg"
@@ -16,38 +16,69 @@ const Appointment = () => {
         {/* Right side (Contact Form) */}
         <div className="md:w-1/2 md:pl-8">
           <h1 className="text-6xl font-bold mb-2 gradient-text">Appointment</h1>
-          <div className="mx-auto border-t-2 border-gray-400 mb-8"></div>  
-          <form> 
-          <div className="mb-4">
-              <label className="block text-black text-sm font-bold mb-2">Name</label>
+          <div className="mx-auto border-t-2 border-gray-400 mb-8"></div>
+          <form>
+            <div className="mb-4">
+              <label className="block text-black text-sm font-bold mb-2">
+                Name
+              </label>
               <input
                 className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
                 type="text"
-                placeholder="Your Name" required
+                placeholder="Your Name"
+                required
               />
             </div>
             <div className="mb-4">
-              <label className="block text-black text-sm font-bold mb-2">Email</label>
+              <label className="block text-black text-sm font-bold mb-2">
+                Email
+              </label>
               <input
                 className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
                 type="email"
-                placeholder="Your Email" required
+                placeholder="Your Email"
+                required
               />
             </div>
             <div className="mb-4">
-              <label className="block text-black text-sm font-bold mb-2">Phone</label>
+              <label className="block text-black text-sm font-bold mb-2">
+                Phone
+              </label>
               <input
                 className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
                 type="tel"
-                placeholder="Your Phone Number" required
+                placeholder="Your Phone Number"
+                required
               />
             </div>
             <div className="mb-4">
-              <label className="block text-black text-sm font-bold mb-2">Time</label>
-              <input
-                className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-                type="time" required
-              />
+              <label className="block text-black text-sm font-bold mb-2">
+                Time
+              </label>
+              <select className="w-full px-3 py-2 text-black border border-gray-300 rounded focus:outline-none focus:border-indigo-500">
+                <option>Select</option>
+                <option>10:00AM</option>
+                <option>10:30AM</option>
+                <option>11:00AM</option>
+                <option>11:30AM</option>
+                <option>12:00AM</option>
+                <option>12:30AM</option>
+                <option>01:00PM</option>
+                <option>01:30PM</option>
+                <option>02:00PM</option>
+                <option>02:30PM</option>
+                <option>03:00PM</option>
+                <option>03:30PM</option>
+                <option>04:00PM</option>
+                <option>04:30PM</option>
+                <option>05:00PM</option>
+                <option>05:30PM</option>
+                <option>06:00PM</option>
+                <option>06:30PM</option>
+                <option>07:00PM</option>
+                <option>07:30PM</option>
+                <option>08:00PM</option>
+              </select>
             </div>
             <button
               className="bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -55,7 +86,7 @@ const Appointment = () => {
             >
               Submit
             </button>
-            </form>
+          </form>
         </div>
       </div>
     </section>
